02e6cf223933d98a5eec2cbaa26a756226c81da0
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-13 17:28:06 -0600

    Create 2_Trapezoid.py

---
+++ 2_Trapezoid.py
@@ -0,0 +1,12 @@
+'''
+TRAPEZOID PROGRAM
+-------------------
+Create a new program that will ask the user for the information needed to find the area of a trapezoid, and then print the area.
+
+Test with the following:
+
+base 1: 2       base 2: 3    height: 4    area: 10
+base 1: 5       base 2: 7    height: 2    area: 12
+base 1: 1       base 2: 2    height: 3    area: 4.5
+base 1: 7       base 2: 2    height: 4    area: 14
+'''
