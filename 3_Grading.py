6899cb9506ee13a7c1b6f356b737515bb8ec017b
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-13 17:35:38 -0600

    Create 3_Grading.py

---
+++ 3_Grading.py
@@ -0,0 +1,14 @@
+'''
+Grading PROGRAM
+---------------
+Create a program that asks the user for their semester grade, final exam grade, 
+and final exam worth and then calculates the overall final grade. 
+Ask your instructor if you don't know how to calculate weighted averages.
+You don't have to print out the letter grade. We will do that in the next chapter.
+
+Test with the following:
+
+Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
+Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
+Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
+'''
