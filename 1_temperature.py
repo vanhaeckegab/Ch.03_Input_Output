736eaf91ce2f944b09ea10736b67d6e54c76b3a4
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-13 17:20:58 -0600

    Create 1_temperature.py

---
+++ 1_temperature.py
@@ -0,0 +1,18 @@
+'''
+TEMPERATURE PROGRAM
+-------------------
+Create a program that asks the user for a temperature in Fahrenheit, and then prints the temperature in Celsius.
+
+Test with the following:
+
+In: 32  Out: 0
+In: 212  Out: 100
+In: 52  Out: 11.1
+In: 25  Out: -3.9
+In: -40  Out: ???
+
+'''
+
+
+
+
