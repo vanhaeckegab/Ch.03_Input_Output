c181812dc5be1dd28e68eb62a2f3b5021494dc63
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-13 18:09:32 -0600

    Create 4_Take_Home_Test.py

---
+++ 4_Take_Home_Test.py
@@ -0,0 +1,102 @@
+# HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
+# but I will definitely not ask another person except the instructor. Signed: Enter name here!
+
+# 1. Write a line of code that will print your name.
+
+
+# 2. How do you enter a comment in a program?
+
+
+
+# 3. If I uncommented the following lines of code what would be their output?
+#print (2/3)
+#print (2//3)
+
+
+# 4. Write a line of code that creates a variable called pi and sets it to an
+#    appropriate value.
+
+
+
+# 5. Why does this code not work?
+A=22
+print(a)
+
+
+# 6. All of the variable names below can be used. But which of these is the best?
+#  a
+#  A
+#  Area
+#  AREA
+#  area
+#  areaOfRectangle
+#  AreaOfRectangle
+
+# 7. Which of these variables names are not allowed in Python?
+#  apple
+#  Apple
+#  APPLE
+#  Apple2
+#  1Apple
+#  account number
+#  account_number
+#  account.number
+#  accountNumber
+#  account#
+
+# 8. Why does this code not work?
+print(a)
+a=45
+
+
+
+# 9. Write a line of code that will ask the user for the length of a square's
+#    side and store the result in a variable. Make sure to convert the value
+#    to an integer.
+
+
+
+# 10. Write a line of code that prints the area of the square, using the
+#     number the user typed in that you stored in question 9.
+
+
+# 11. Do the same as in questions 9 and 10, but with the formula for the
+#     area of an ellipse.
+#     Area=pi*a*b
+#     where a and b are the lengths of the major radii.
+
+
+
+# 12. Do the same as in questions 9 and 10, but with a formula to find
+#     the pressure of a gas.
+#     PV=nRT
+#     where n is the number of moles, T is the absolute temperature, V is the
+#     volume, and R is the gas constant 8.3144.
+
+
+
+# 13. Explain the mistake in this code:
+pi = float(3.14)
+
+
+
+
+# 14. Assume radius has been defined.
+#    This code runs, but isn't very good. Explain the mistake in the following code:
+x=3.14
+pi=x
+area=pi*radius**2
+
+
+
+# 15. Assuming x and y are already defined, this code runs. But
+#    something isn't quite right. Explain the mistake in the following code:
+a=((x)*(y))
+
+
+
+# 16. Explain the mistake in the following code:
+radius = input(float("Enter the radius:"))
+
+
+
