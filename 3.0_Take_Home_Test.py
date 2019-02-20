96672d5238d848945395ffa210f41566f3cffccc
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-19 22:53:57 -0600

    Update 3.0_Take_Home_Test.py

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -1,5 +1,7 @@
-
-#HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, but I will definitely not ask another person except the instructor. Signed: ______________________
+'''
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, 
+but I will definitely not ask another person except the instructor. Signed: ______________________
+'''
 
 # 1.) Write a program that asks someone for their name and then prints their name to the screen?
 
