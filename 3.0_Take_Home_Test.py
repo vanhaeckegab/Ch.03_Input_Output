8095fb51271cbc255f4921bda1a54f6d786ff3ee
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-19 22:34:42 -0600

    Update 3.0_Take_Home_Test.py

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -1,68 +1,19 @@
-'''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+
+#HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, but I will definitely not ask another person except the instructor. Signed: ______________________
+
+# 1.) Write a program that asks someone for their name and then prints their name to the screen?
 
 
-
-1. Write a line of code that will print your name.
-'''
+# 2. Write a a program where a user enters a base and height and you print the area of a triangle.
 
 
-'''
-2. Write a program that asks someone for their name and then prints their name to the screen?
-
-'''
+# 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
 
-'''
-3. Predict the output of the following lines of code and then test them? Write down your prediction and the output.
-print (17/9)
-print (17//9)
-print (17%9)
-'''
+# 4. Ask a user for an integer and then print the square root.
 
 
-'''
-4. Write a a program where a user enters a base and height and you print the area of a triangle.
+# 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
 
-'''
-
-
-'''
-5. Change this program so it works.
-A=May the Force be with you!
-print(a)
-'''
-
-
-
-
-'''
-6. Write a line of code that will ask the user for the length of a square's
-side and then print the area of the square
-'''
-
-
-
-'''7. Ask a user for the length of radii of an ellipse and then print its area. 
-Area=pi*a*b where a and b are the lengths of the major radii.
-'''
-
-
-
-'''
-8. Ask a user for moles, volume and temperature of a gas and print out the pressure. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
-volume, and R is the gas constant 8.3144.
-'''
-
-'''
-9. Ask a user for an integer and then print the square root.
-'''
-
-
-'''
-10. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration
-and then print out the Force on one line and "Get it?" on the next.
-'''
 
 
