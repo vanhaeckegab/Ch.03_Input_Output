57baf6b083e9f07254cf93261c0c7367a8a07f77
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 12:45:34 -0500

    Update and rename 3.0_Take_Home_Test.py to 3.0_Jedi_Training.py

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,7 +1,5 @@
-'''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, 
-but I will definitely not ask another person except the instructor. Signed: ______________________
-'''
+#Sign your name:________________
+
 
 # 1.) Write a program that asks someone for their name and then prints their name to the screen?
 
